523504f85ceb8053f4e8a0cbe69b09c4c97e4017
Duvier Reinosa <duvierreinosa@gmail.com>
2022-03-27 23:59:46 -0500

    validación para iconos

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -8,7 +8,7 @@ import plusIcon from '../assets/static/plus-icon.png';
 import removeIcon from '../assets/static/remove-icon.png'
 
 const CarouselItem = (props) => {
-  const { id,cover, title, year, contentRating, duration } = props;
+  const { id,cover, title, year, contentRating, duration, isList } = props;
   const handleSetFavorite = () => {
     props.setFavorite({
       id, cover, title, year, contentRating, duration
@@ -23,16 +23,18 @@ const CarouselItem = (props) => {
       <div className="carousel-item__details">
         <div>
           <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" />
-          <img 
+          {
+            isList ? <img 
+            src={removeIcon} 
+            className="carousel-item__details--img" 
+            alt="" 
+            onClick={()=>handleDeleteFavorite(id)}/> :
+            <img 
             className="carousel-item__details--img" 
             src={plusIcon} 
             alt="Plus Icon"
             onClick={handleSetFavorite} />
-            <img 
-              src={removeIcon} 
-              className="carousel-item__details--img" 
-              alt="" 
-              onClick={()=>handleDeleteFavorite(id)}/>
+          }
         </div>
         <p className="carousel-item__details--title">{title}</p>
         <p className="carousel-item__details--subtitle">
